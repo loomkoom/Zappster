2502544fe84a0f5c47519b029fad5e1b5a42eaba
loomkoom <29258685+loomkoom@users.noreply.github.com>
2022-05-25 15:27:39 +0200

    mail image

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ import sqlite3
 import string
 
 import requests
+import os
 from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
@@ -56,13 +57,14 @@ def wish_form(card_id):
     return render_template("send_wish.html", card=card)
 
 
-def send_mail(email, uid):
+def send_mail(email, sender, cardid, uid):
     x = requests.post("https://api.mailgun.net/v3/sandboxa39931aba4ea43a885c240d815b0a2c2.mailgun.org/messages",
                       auth=("api", "9374b99615d0f43ff1e12995ef3c3317-8d821f0c-6df1ae28"),
+                      files=[('inline[0]', ('card.jpg', open(f'{os.getcwd()}/static/{cardid}.jpg', mode='rb').read()))],
                       data={"from": "laurens@zappsters.pythonanywhere.com",
                             "to": [f"{email}"],
                             "subject": "Greeting card",
-                            "text": "You have been sent an AR greeting card! The unique code for your personnel message is!" + uid})
+                            "text": f"You have been sent an AR greeting card from {sender}! The unique code for your personnel message i: " + uid})
     return x.text
 
 
@@ -87,7 +89,7 @@ def wish_insert():
     uid = generate_uid(db_id)
 
     if send_method == "email":
-        send_mail(send_destination,uid)
+        send_mail(send_destination,sender,card_id, uid)
 
     card = cur.execute('SELECT * FROM card WHERE cardid' + '=' + str(card_id) + ';').fetchone()
     conn.execute(
